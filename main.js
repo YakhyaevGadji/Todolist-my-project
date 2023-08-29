e4556028afec0a8fff974baba761121678615e96
gadji <gadji180105@gmail.com>
2023-08-29 09:04:30 +0300

    update todolist

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 const from = document.querySelector('.form');
 const input = document.querySelector('#input');
 const list = document.querySelector('#list');
+const allBtn = document.querySelector('[data-card="all"]');
+const deleteBtn = document.querySelector('[data-card="delete"]');
+const favoriteBtn = document.querySelector('[data-card="favorite"]');
+const buttonsRow = document.querySelectorAll('.header__list-item');
+const filterBtn = document.querySelector('#filter');
+const filterList = document.querySelector('.filter__list');
+const filters = document.querySelectorAll('[data-filter]');
 
 from.addEventListener('submit', addNewTask);
 list.addEventListener('click', taskDone);
 list.addEventListener('dblclick', chengeTextInTask);
-// list.addEventListener('click', taskDelete);
+list.addEventListener('click', taskDelete);
+list.addEventListener('click', taskFaforite);
 
-let tasks = [];
+deleteBtn.addEventListener('click', renderDeleteTask);
+allBtn.addEventListener('click', renderAllTask);
+favoriteBtn.addEventListener('click', renderFavoriteTask);
+filterBtn.addEventListener('click', styleActiveInFilter);
+
+filters.forEach((filter) => {
+    filter.addEventListener('click', filterTaskList);
+});
+
+let date = {
+    all: [],
+    deleted: [],
+    favorites: []
+};
 
 function addNewTask(event) {
     event.preventDefault();
@@ -19,24 +40,39 @@ function addNewTask(event) {
         favorites: false,
         deleted: false
     }
-   
-    const cssTask = taskInfo.done ? 'task__text_active' : 'task__text';
+    
+    list.insertAdjacentHTML('beforeend', renderHtmlTask(taskInfo));
 
+    date.all.push(taskInfo);
+    
+    input.value = '';
+}
+
+function renderHtmlTask(taskInfo) {
+    const cssTask = taskInfo.done ? 'task__text_active' : 'task__text';
+    let check = '';
+    let favorit = '';
+
+    if(taskInfo.done) {
+        check = 'checked';
+    }
+
+    if(taskInfo.favorites) {
+        favorit = 'checked';
+    }
+    
     const taskHTML = `<li class="task__item" id="${taskInfo.id}">
         <div class="task__block">
-            <input type="checkbox" class="task__checkbox" data-action="done">
+            <input type="checkbox" class="task__checkbox" ${check}  data-action="done">
             <p class="${cssTask}" data-action="taskText">${taskInfo.title}</p>
         </div>
          <div class="setting">
             <button class="task__remove" data-action="delete">delete</button>
-            <input type="checkbox" data-action="favorites">
+            <input type="checkbox" ${favorit} data-action="favorites">
         </div>
     </li`;
 
-    list.insertAdjacentHTML('beforeend', taskHTML);
-
-    tasks.push(taskInfo);
-    input.value = '';
+    return taskHTML;
 }
 
 function taskDone(event) {
@@ -45,7 +81,7 @@ function taskDone(event) {
         const parentId = Number(parent.id);
         const taskTitle = parent.querySelector('[data-action="taskText"]');
 
-        tasks.findIndex((item) => {
+        date.all.findIndex((item) => {
             if(parentId === item.id) {
                 item.done = !item.done;
             }
@@ -68,7 +104,7 @@ function chengeTextInTask(event) {
         taskNewInput.addEventListener('blur', function() {
             taskTitle.textContent = taskNewInput.value;
 
-            tasks.forEach((item) => {
+            date.all.forEach((item) => {
                 if(parentId === item.id) {
                     item.title = taskNewInput.value;
                 }
@@ -79,3 +115,110 @@ function chengeTextInTask(event) {
         taskNewInput.focus();
     }
 }
+
+function taskDelete(event) {
+    if(event.target.dataset.action === 'delete') {
+        const parentEl = event.target.closest('.task__item');
+        const parentId = Number(parentEl.id);
+        
+        const taskIndex = date.all.findIndex((item) => {
+            if(item.id === parentId) {
+                item.deleted = !item.deleted;
+                date.deleted.push(item);
+                return true;
+            }
+        });
+
+        date.all.splice(taskIndex, 1);
+        parentEl.remove();
+    }
+}
+
+function taskFaforite(event) {
+    if(event.target.dataset.action === 'favorites') {
+        const parentEl = event.target.closest('.task__item');
+        const parentId = Number(parentEl.id);
+
+        date.all.forEach((item) => {
+            if(parentId === item.id) {
+                if(item.favorites) {
+                    item.favorites = false;
+
+                    date.favorites.forEach((item, index) => {
+                        if(parentId === item.id) {
+                            date.favorites.splice(index, 1);
+                        }
+                    });
+                }else {
+                    item.favorites = true;
+                    item.favorites.push(item);
+                }
+            }
+        });
+        
+    }
+}
+function renderDeleteTask() {
+    const childrens = list.querySelectorAll('.task__item');
+    
+    childrens.forEach((item) => {
+        item.remove();
+    });
+    
+    date.deleted.forEach((item) => {
+        list.insertAdjacentHTML('beforeend', renderHtmlTask(item));
+    });
+
+    activeButtonTask(this);
+}
+function renderAllTask() {
+    const childrens = list.querySelectorAll('.task__item');
+    
+    childrens.forEach((item) => {
+        item.remove();
+    });
+    
+    date.all.forEach((item) => {
+        list.insertAdjacentHTML('beforeend', renderHtmlTask(item));
+    });
+
+    activeButtonTask(this);
+}
+function renderFavoriteTask() {
+    const childrens = list.querySelectorAll('.task__item');
+    
+    childrens.forEach((item) => {
+        item.remove();
+    });
+    
+    date.favorites.forEach((item) => {
+        list.insertAdjacentHTML('beforeend', renderHtmlTask(item));
+    });
+    
+    activeButtonTask(this);
+}
+function activeButtonTask(elem) {
+    buttonsRow.forEach((item) => {
+        item.classList.remove('header__list-item__active');
+    });
+    elem.classList.add('header__list-item__active');
+}
+function styleActiveInFilter() {
+    if(filterList.classList[1] === 'filter__list_active') {
+        filterList.classList.remove('filter__list_active');
+    }else {
+        filterList.classList.add('filter__list_active');
+    }
+}
+function filterTaskList() {
+    if(this.dataset.filter === 'new') {
+        date.all.forEach((item) => {
+            
+        });
+    }else {
+        
+    }
+    filterList.classList.toggle('filter__list_active');
+}
+
+
